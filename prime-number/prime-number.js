10d44b0b493719b2345c475fd7823e9749d231a5
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-12-03 15:39:58 -0500

    added prime code

--- prime-number/prime-number.js
+++ prime-number/prime-number.js
@@ -1,1 +1,18 @@
 //check if a number is a prime number:
+function test_prime(n) {
+  if (n === 1) {
+    return "n: " + n + " is NOT a Prime Number .. " + false;
+  } else if (n === 2) {
+    return "n: " + n + " is a Prime Number! " + true;
+  } else {
+    for (var x = 2; x < n; x++) {
+      if (n % x === 0) {
+        return "n: " + n + " is NOT a Prime Number .. " + false;
+      }
+    }
+    return "n: " + n + " is a Prime Number .. " + true;
+  }
+}
+
+//trigger for checking if a number is a prime number:
+//console.log(test_prime(12));
