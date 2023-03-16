5133cc266534937a6ed5af53179c91ee14a766eb
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2023-03-15 21:45:09 -0400

    fixed function to be for min prod

--- various_sample_testing_only/various_sample_testing_only.js
+++ various_sample_testing_only/various_sample_testing_only.js
@@ -17,3 +17,20 @@ let arr5 = [3, 6, -2, -5, 7, 3];
 console.log("sliced 0, -1 is:" + arr5.slice(0, -1));
 
 console.log("solution:" + solution(arr5));
+
+function adjacentElementsMinProduct(inputArray) {
+  var curr = inputArray[0] * inputArray[1];
+  var prod = curr;
+  for (var i = 1; i < inputArray.length; i++) {
+    console.log("curr:" + curr);
+    var curr = inputArray[i] * inputArray[i + 1];
+    if (curr > prod) {
+      prod = curr;
+      console.log("prod:" + prod);
+    }
+  }
+  return prod;
+}
+console.log(
+  "minimum product = " + adjacentElementsMinProduct([-23, 4, -3, 8, -12])
+);
